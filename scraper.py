1ce87475fd51de804b1f17bc87090c918094e6b9
woodbine <ian@spendnetwork.com>
2015-04-21 16:03:08 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,8 +6,8 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 # Set up variables
-entity_id = "E1221_DCC_gov"
-url = "http://www.dorsetforyou.com"
+entity_id = "E1121_DCC_gov"
+url = "http://www.devon.gov.uk/index/councildemocracy/councilfinance/expenditure-over500.htm"
 
 # Set up functions
 def convert_mth_strings ( mth_string ):
@@ -20,32 +20,32 @@ def convert_mth_strings ( mth_string ):
 
 # pull down the content from the webpage
 html = urllib2.urlopen(url)
-print html
 soup = BeautifulSoup(html)
-print soup
+
 # find all entries with the required class
-pageLinks = soup.findAll('a', href=True)
+block = soup.find('div',{'id':'list'})
+pageLinks = block.findAll('a', href=True)
 
 for pageLink in pageLinks:
-	pageUrl = 'http://www.dorsetforyou.com/' + link['href']
-	if 'Expenditure' in pageUrl:
-		html2 = urllib2.urlopen(pageUrl)
-		soup2 = BeautifulSoup(html2)
-		
-		yrBlock = soup2.find('div',{'id':'download'})
-		fileLinks = yrBlock.findAll('a',href=True)
-		
-		for fileLink in fileLinks:
-			url = 'http://www.dorsetforyou.com/' + fileLink['href']
-			if '.csv' in url:
-				#  clean up the onclick data
-				title = link.contents[0]
-				# create the right strings for the new filename
-				csvYr = title.split(' ')[-1]
-				csvMth = title.split(' ')[-2][:3]
-				csvMth = csvMth.upper()
-				csvMth = convert_mth_strings(csvMth);
-				filename = entity_id + "_" + csvYr + "_" + csvMth + ".csv"
-				todays_date = str(datetime.now())
-				scraperwiki.sqlite.save(unique_keys=['l'], data={"l": url, "f": filename, "d": todays_date })
-				print filename
+	print pageLink
+	pageUrl = 'http://www.devon.gov.uk/' + link['href']
+	html2 = urllib2.urlopen(pageUrl)
+	soup2 = BeautifulSoup(html2)
+	
+	yrBlock = soup2.find('div',{'id':'download'})
+	fileLinks = yrBlock.findAll('a',href=True)
+	
+	for fileLink in fileLinks:
+		url = 'http://www.devon.gov.uk/' + fileLink['href']
+		if '.csv' in url:
+			#  clean up the onclick data
+			title = link.contents[0]
+			# create the right strings for the new filename
+			csvYr = title.split(' ')[-1]
+			csvMth = title.split(' ')[-2][:3]
+			csvMth = csvMth.upper()
+			csvMth = convert_mth_strings(csvMth);
+			filename = entity_id + "_" + csvYr + "_" + csvMth + ".csv"
+			todays_date = str(datetime.now())
+			scraperwiki.sqlite.save(unique_keys=['l'], data={"l": url, "f": filename, "d": todays_date })
+			print filename
